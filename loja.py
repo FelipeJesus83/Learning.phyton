33de1a931c42fb331a9d75fdc94ae5919345fd1a
Felipe.Jesus.Dev <i7rdddr@gmail.com>
2026-09-21 22:30:55 -0300

    Loja eletronica ATUALIZADA
    atualizei e coloquei descontos

--- loja.py
+++ loja.py
@@ -24,18 +24,42 @@ if escolha2 == '1':
     if prdto in produtos:
         quantas2 = int(input(f'Quantos {prdto} você quer: '))
         preço2 = produtos[prdto]
+        if preço2 > 6000:
+            desconto2 = 0.20
+            totalr = totalf + (preço2 * quantas2) * (1 - desconto2)
+        else:
+            totalr = totalf + preço2 * quantas2
+
         print(f'O seu {prdto}, custa R${preço2}')
-        totalr = totalf + preço2 * quantas2
         print(f'Seu novo total é {totalr} ')
         print('=== RESUMO ===\n')
         print(f'{escolha} x{quantas} = R${preço}')
         print(f'{escolha2} x{quantas2} = R${preço2}')
         print(f'O total geral: R${totalr}')
+
+        if total > 6000 and preço2 * quantas2 > 6000:
+            print('O desconto aplicado total foi de 40%')
+        elif total > 6000:
+            print('Sua primeira compra atingiu 6k,e você ganhou desconto de 20%')
+        elif preço2 * quantas2 > 6000:
+            print('Sua segunda compra atingiu 6k,e você ganhou no total 20%  De desconto ')
+        else:
+            print('Você não teve desconto')
  
     else:
         print('Produto inexistente')
+        
 else:
     print('Até mais,aqui está seu resumo\n')
     print('=== RESUMO ===')
     print(f'{escolha} x{quantas} = R${preço}')
     print(f'O total é: {totalf}')
+
+desconto_primeira = total - totalf
+
+print(f'Desconto 1ª compra: R${desconto_primeira}')
+print(f'Você economizou no total: R${(total + preço2 * quantas2) - totalr}')
+
+sair = int(input('Deseja sair: (Sim = 1)(Não = NÃO RESPONDA)'))
+if sair == 1:
+    print('Até mais')
